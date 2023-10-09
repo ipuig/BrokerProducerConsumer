fe656032d0839668814f024284104698ce346104
Ivan Puig <ivan@bpuig.com>
2023-10-09 22:02:24 +0100

    Add Producer and Consumer

---
+++ src/Producer.java
@@ -0,0 +1,63 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.concurrent.Executors;
+
+public class Producer extends NNode {
+
+    public Producer() {
+        try {
+            this.serverSocket = new DatagramSocket();
+            this.threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void start() {
+
+
+        while(true) {
+
+        }
+
+    }
+
+    public void send() {
+
+    }
+
+    public void receive() {
+
+    }
+
+    private static class ProducerHandler extends Handler {
+
+        public ProducerHandler(DatagramSocket server, DatagramSocket receive) {
+            super(server, receive);
+        }
+        
+
+        public void run() {
+
+            try {
+
+                // TODO: process request
+                
+                // TODO: extract binary header and encoded data
+                
+                // TODO: decode and handle the binary header as needed
+                
+                // TODO: Handle responses according to header
+                
+                // TODO: send an acknowledgement back to the sender
+                
+            } catch (Exception e) {
+
+            }
+
+        }
+
+    }
+    
+}
