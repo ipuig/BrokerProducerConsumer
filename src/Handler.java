d8ea7cd3e0fd27f2b60652b4b4a40a13925b6e19
Ivan Puig <ivan@bpuig.com>
2023-10-10 12:27:18 +0100

    Update handler to match guidelines

--- src/Handler.java
+++ src/Handler.java
@@ -16,22 +16,77 @@ public abstract class Handler {
             byte receivedStreamIdentifier, byte[] receivedPayloadLabel, byte[] payload) {
 
             switch(receivedPacketType) {
-                default:
+                case 10:
                     System.out.println("Received Packet Type: PUBLISH");
+                    break;
+                case 60:
+                    System.out.println("Received ACK from Broker");
+                    break;
+                case 11:
+                    System.out.println("Received Packet Type: LREQUEST");
+                    break;
+                case 61:
+                    System.out.println("Received ACK from Broker");
+                    break;
+                case 12:
+                    System.out.println("Received Packet Type: LDATA");
+                    break;
+                case 62:
+                    System.out.println("Received ACK from Consumer");
+                    break;
+                case 13:
+                    System.out.println("Received Packet Type: SUBSCRIBE");
+                    break;
+                case 63:
+                    System.out.println("Received ACK from Broker");
+                    break;
+                case 14:
+                    System.out.println("Received Packet Type: UNSUBSCRIBE");
+                    break;
+                case 64:
+                    System.out.println("Received ACK from Broker");
+                    break;
+                case 15:
+                    System.out.println("Received Packet Type: FORWARD");
+                    break;
+                case 65:
+                    System.out.println("Received ACK from Consumer");
                     break;
 
             }
 
-            System.out.printf("Received Producer Identifier: %x_%x_%x \n",
-                    receivedProducerIdentifier[0],
-                    receivedProducerIdentifier[1],
-                    receivedProducerIdentifier[2]);
+            switch(receivedProducerIdentifier[0]) {
+                default:
+                    break;
+                case 0xA: 
+                    System.out.print("Received Broker Id: 0xA:");
+                    break;
+                case 0xC: 
+                    System.out.print("Received Consumer Id: 0xC:");
+                    break;
+                case 0xE: 
+                    System.out.print("Received Producer Id: 0xE:");
+                    break;
 
-            System.out.printf("Stream identifier %x\n", receivedStreamIdentifier);
+            }
+            System.out.printf("%x:%x\n", receivedProducerIdentifier[1], receivedProducerIdentifier[2]);
+
+            if(receivedStreamIdentifier > 0) System.out.printf("Stream identifier %d\n", receivedStreamIdentifier);
 
             switch(receivedPayloadLabel[0]) {
                 default:
+                    break;
+                case 0xA:
                     System.out.printf("Received Payload Label: [Audio, %x]\n", receivedPayloadLabel[2]);
+                    break;
+                case 0xF:
+                    System.out.printf("Received Payload Label: [Frame, %x]\n", receivedPayloadLabel[2]);
+                    break;
+                case 0xE:
+                    System.out.printf("Received Payload Label: [List] of size %x\n", receivedPayloadLabel[2]);
+                    break;
+                case 0x9:
+                    System.out.println("Received Payload Label: [Producer]");
                     break;
 
             }
